9a4ecfb2c5cd6187b25d4eb843888590b3306163
mac <mac@macui-MacBookPro.local>
2024-02-25 22:22:08 +0900

    feat: H-TASK

--- src/train.ts
+++ src/train.ts
@@ -7,25 +7,42 @@ MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 */
 
 
-function getHighestIndex(arr: number[]): number {
-    if (arr.length === 0) {
-      return -1;
-    }
+// function getHighestIndex(arr: number[]): number {
+//     if (arr.length === 0) {
+//       return -1;
+//     }
   
-    let maxIndex = 0;
-    let maxValue = arr[0]; 
+//     let maxIndex = 0;
+//     let maxValue = arr[0]; 
   
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] > maxValue) {
-        maxValue = arr[i];
-        maxIndex = i;
-      }
-    }
+//     for (let i = 1; i < arr.length; i++) {
+//       if (arr[i] > maxValue) {
+//         maxValue = arr[i];
+//         maxIndex = i;
+//       }
+//     }
   
-    return maxIndex;
-  }
+//     return maxIndex;
+//   }
   
-  console.log(getHighestIndex([5, 0, 12, 20, 84])); 
+//   console.log(getHighestIndex([5, 0, 12, 20, 84])); 
   
-  console.log("test"); 
+//   console.log("test"); 
   
+
+
+
+
+/*H-TASK: 
+
+Shunday function tuzing, u string qabul qilib teskari qilib return qilsin
+MASALAN: gerReverse("hello") return qiladi "olleh"
+
+*/
+
+
+function getReverse(soz: string){
+   return soz.split('').reverse().join("")
+}
+
+console.log(getReverse("Assalomu alaykum"));
