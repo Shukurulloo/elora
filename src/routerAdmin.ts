55d83f0d728064eee26f7db8f8ac4cdbf757dc30
mac <mac@macui-MacBookPro.local>
2024-03-01 16:51:32 +0900

    feat: created restaurant member related router get and post endpoints

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -2,10 +2,16 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 
-routerAdmin.get("/", restaurantController.goHome);
+/** Restaurant */
+routerAdmin.get("/", restaurantController.goHome);                            // Minimalistic pathot => get(), post()  ko'p ishlatamiz
+routerAdmin
+.get("/login", restaurantController.getLogin)
+.post("/login", restaurantController.processLogin);                    // get() pagega kirish malumot olish uchun // post() malumotni o'zgartirish uchun    
+routerAdmin
+.get("/signup", restaurantController.getSignup)
+.post("/signup", restaurantController.processSignup)
 
-routerAdmin.get("/login", restaurantController.getLogin);
-
-routerAdmin.get("/signup", restaurantController.getSignup);
+/** Product */
+/** User */
 
 export default routerAdmin;
