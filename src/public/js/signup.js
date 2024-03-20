754f1a7ad016c1238821595213a5c070999c4281
mac <mac@macui-MacBookPro.local>
2024-03-20 19:22:55 +0900

    fix: finish frontend restaurant signup process

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,29 @@
 console.log("Signup frontend javascript file");
 
-$(function () {});
+$(function () { //prevender mantig'i
+    const fileTarget = $(".file-box .upload-hidden");
+    let fileneme;
+
+    fileTarget.on("change", function () {
+    if(window.FileReader) {
+        const uploadFile = $(this)[0].files[0];
+        const fileType = uploadFile["type"];
+        const validImageType = ["image/jpg", "image/jpeg", "image/png"]; // main type
+        if(!validImageType.includes(fileType)) {
+            alert(" Please insert only jpeg, jpg, png!")
+        } else {
+            if(uploadFile) {
+                console.log(URL.createObjectURL(uploadFile));
+                $(".upload-img-frame")
+                    .attr("src", URL.createObjectURL(uploadFile))
+                    .addClass("success")
+            }
+            filename = $(this)[0].files[0].name;
+        }
+        $(this).siblings(".upload-name").val(filename); // defoult rasmni member rasmga almashtirish
+     }
+    });
+});
 
 function validateSignupForm() {
     const memberNick = $(".member-nick").val();
@@ -20,6 +43,14 @@ function validateSignupForm() {
 
     if (memberPassword !== confirmPassword) {
         alert("Password differs, please check!")
+        return false;
+    }
+
+    const memberImage = $(".member-image").get(0).files[0] 
+    ? $(".member-image").get(0).files[0].name 
+    : null;
+    if(!memberImage) {
+        alert("Please insert restaurant image!");
         return false;
     }
 }
