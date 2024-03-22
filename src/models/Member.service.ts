3f72d32cad18418900af5c830379b90bb84a831b
mac <mac@macui-MacBookPro.local>
2024-03-22 18:57:03 +0900

    fix: modify spa login rest api process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,7 +1,7 @@
 import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput, MemberUpdateInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
-import { MemberType } from "../libs/enums/member.enum";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
 import * as bcrypt from "bcryptjs";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 
@@ -32,12 +32,15 @@ class MemberService {
         // TODO: Consider member status later
         const member = await this.memberModel
             .findOne(
-                {memberNick: input.memberNick}, // search
-                {memberNick: 1, memberPassword: 1} // obtions
+                {memberNick: input.memberNick, memberStatus: {$ne: MemberStatus.DELETE }}, // search
+                {memberNick: 1, memberPassword: 1 , memberStatus: 1} // obtions
             )
             .exec();
         if(!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
-
+        else if(member.memberStatus === MemberStatus.BLOCK){
+            throw new Errors(HttpCode.FORBIDDEN, Message.BLOCKED_USER)
+        }
+        console.log("member:", member);
             const isMatch = await bcrypt.compare(
                 input.memberPassword, 
                 member.memberPassword
